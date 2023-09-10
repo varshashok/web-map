b6c0939e019bd4db3a6f77bdc1e8ed23b81f50e9
varsha <inuvarsha@gmail.com>
2023-09-10 17:46:24 -0300

    my first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiaW51YXNob2siLCJhIjoiY2xrOHY5ZDIwMGNnODNmcXJhcWlvMm5jciJ9.wk2kUfNF27VRZ8_p9nh2uA";
+const map = new mapboxgl.Map({
+container: "map", // container ID
+style: "mapbox://styles/inuashok/clka64st103cs01p7be6ieuw0", // style URL
+center: [-73.984, 40.756], // starting position [lng, lat]
+zoom: 15.35, // starting zoom
+});
